6744c1448d590931eae2745562f8527067092c6d
Alginatic <203040094@mail.unpas.ac.id>
2022-12-22 02:29:25 +0700

    rev

--- shelter-laravel/resources/views/layouts/main.blade.php
+++ shelter-laravel/resources/views/layouts/main.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
     <link rel="stylesheet" href="/css/style.css">
+    <link href="css/{{ ('about.css') }}" rel="stylesheet">
 
     <title>.conf | {{ $title }} </title>
   </head>
@@ -24,7 +25,7 @@
         @yield('container')
     </div>
 
-   
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
