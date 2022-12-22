e876cd4f2aaa745ffc1cd4acf35c5f7d62e38ecc
QueenAgella <hervinfakhrulm@gmail.com>
2022-12-23 03:23:54 +0700

    rev

--- shelter-laravel/resources/views/home.blade.php
+++ shelter-laravel/resources/views/home.blade.php
@@ -6,9 +6,9 @@
     <div class="container mt-2">
         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="4000">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" style="background-color:black"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" style="background-color:black"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" style="background-color:black"></button>
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
