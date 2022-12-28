e4210eb3c80448f6961b5417bd93b77467a76926
QueenAgella <hervinfakhrulm@gmail.com>
2022-12-29 00:36:02 +0700

    rev about $ web route

--- shelter-laravel/routes/web.php
+++ shelter-laravel/routes/web.php
@@ -28,10 +28,46 @@ Route::get('/', function () {
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "about",
+        "title" => "About Us",
         "active" => 'about',
+
+        'about' => '.conf adalah sebuah website yang menyediakan berbagai macam jenis sepatu mulai dari Adidas, Nike sampai Puma, kami menjual sepatu yang berkualitas dn nyaman saat digunakan. Beberapa keungulan dari sepatu yang kami jual adalah :',
+        'one' => '- Harga Terjangkau',
+        'two' => '- Kualitas Terbaik',
+        'three' => '- Bervariatif',
+        'about2' => '.conf memberikan harga tetap terjangkau tanpa mengurangi kualitas produknya. Kami bertujuan untuk menciptakan seni dalam bentuk sepatu. Kami percaya bahwa setiap pasangan adalah ekspresi yang tidak dapat disalin atau dibuat ulang. Kami ingin berbagi berbagai bentuk seni yang mewakili ANDA.',
+
+        'product' => 'Aneka produk sepatu yang kami sediakan.',
+        'product2' => 'Selain harga yang terjangkau dan kualitas yang baik, .conf memiliki banyak pilihan jenis dan model sehingga Anda mungkin tidak akan bosan.',
+        'product3' => 'Adidas',
+        'product4' => 'Nike',
+        'product5' => 'Puma',
+
         "name" => 'Hervin Fakhrul',
-        
+        'job' => 'Back End',
+        'image' => 'hervin.jpeg',
+        'quote' => 'Saya tidak menyangka dengan harga semurah itu saya bisa mendapatkan sepatu original dengan kualitas sebagus ini, top 10 bintang untuk penjualnya.',
+
+        "name2" => 'Kelvin Ardiansyah',
+        'job2' => 'Team Leader',
+        'image2' => 'kelvin.jpeg',
+        'quote2' => 'Wah barangnya bagus banget, nyaman dipakai, ga nyesel belanja disini, terpercaya banget, harga juga terjangkau, penjualnya juga ramah, chat langsung dibalas.',
+
+        "name3" => 'Rizky Mahendra',
+        'job3' => 'Back End',
+        'image3' => 'mahendra.jpeg',
+        'quote3' => 'Sangat cantik. Ukurannya juga sempurna. Sepertinya kualitas ini akan bertahan sangat lama. Terima kasih!',
+
+        "name4" => 'Yulius Yogi Yuwono',
+        'job4' => 'Front End',
+        'image4' => 'yulius.jpeg',
+        'quote4' => 'Pengirimannya cepat, beli produk disini sangat berbaloi, harga terjangkau, adminnya responsif banget, recommended banget belanja di toko ini.',
+
+        "name5" => 'Religi Realista Esthetika',
+        'job5' => 'Front End',
+        'image5' => 'religi.jpeg',
+        'quote5' => 'Keracunan temen yang membeli produk ini karena pas sampai barangnya bagus jadi langsung checkout, tidak mengecewakan. Sangat direkomendasikan.'
+
     ]);
 });
 
