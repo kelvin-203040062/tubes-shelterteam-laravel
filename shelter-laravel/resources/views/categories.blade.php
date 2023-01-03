3f22038dade88ddb66ddd6485b6bb70a75c85f66
kelvin-203040062 <75973924+kelvin-203040062@users.noreply.github.com>
2023-01-03 11:02:32 +0700

    memperbaiki responsive dashboar serta navbar

--- shelter-laravel/resources/views/categories.blade.php
+++ shelter-laravel/resources/views/categories.blade.php
@@ -32,7 +32,7 @@
 <div class="container">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}">
-<link href="css/{{ ('responcategories.css') }}" rel="stylesheet">
+<link href="css/{{ ('categories.css') }}" rel="stylesheet">
 <script src="{{ url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i') }}" rel="stylesheet" crossorigin="anonymous"></script>
 
 @endsection
